5e4a9e98d4cca1c895f11fd1c595cedef7a37e2b
sukhitashvili <sukhitashvili.soso@gmail.com>
2025-05-06 22:30:22 +0200

    achieved to read general information from a PDF

--- document_extractor.py
+++ document_extractor.py
@@ -3,11 +3,12 @@ import re
 from pathlib import Path
 
 import PIL
+import numpy as np
 import pandas as pd
 import pymupdf
 from pydantic_core import from_json
 
-from model_inference import clean_cuda_mem
+from model_inference import ModelInference
 
 
 class DocumentExtractor:
@@ -20,32 +21,32 @@ class DocumentExtractor:
         self.document_types = list(self.extraction_fields.keys())
         self.document_types.pop(0)
         self.document_types = [' '.join(i.split('_')).title() for i in self.document_types]  # beautify the text
-        # self.vision_llm = ModelInference(path=self.model_id, device=self.device)
-        self.image_text_extraction_prompt = [
-            {"role": "system", "content": [{"type": "text", "text": "You are a helpful assistant."}]},
-            {"role": "user", "content": [
-                {"type": "image"},
-                {"type": "text",
-                 "text": "Extract all the text line by line from the image."}
-            ]}
-        ]
+        self.vision_llm = ModelInference(path=self.model_id, device=self.device)
+        # self.image_text_extraction_prompt = [
+        #     {"role": "system", "content": [{"type": "text", "text": "You are a helpful assistant."}]},
+        #     {"role": "user", "content": [
+        #         {"type": "image"},
+        #         {"type": "text",
+        #          "text": "Extract all the text line by line from the image."}
+        #     ]}
+        # ]
 
     def extract(self, document_path: str):
         doc = pymupdf.open(document_path)
         document_pages = doc.page_count
-        whole_document_text = ''
+        if document_pages > 2: raise ValueError('Document pages should not be more than 2 for performance reasons.')
+        whole_document_image = None
         for page in range(document_pages):
             doc_page = doc.load_page(page)
             doc_page_pil_img = self.convert_to_img(doc_page)
-            doc_page_text = self.extract_text_from_image(doc_page_pil_img)
-            whole_document_text += doc_page_text if whole_document_text == '' else f'\n{doc_page_text}'
+            if whole_document_image is None:
+                whole_document_image = np.array(doc_page_pil_img)
+            else:
+                whole_document_image = np.concatenate((whole_document_image, doc_page_pil_img), axis=0)
 
-        document_language = self.get_text_language(document_text=whole_document_text)
+        general_info = self.get_general_information(pil_img=whole_document_image)
 
-    def extract_text_from_image(self, pil_img: PIL.Image.Image) -> str:
-        img_text = self.vision_llm(messages=self.image_text_extraction_prompt, images=[pil_img], max_new_tokens=2000)[0]
-        clean_cuda_mem()
-        return img_text
+        print(general_info)
 
     def get_text_language(self, document_text: str) -> str:
         prompt = [{"role": "system", "content": [{"type": "text", "text": "You are a helpful assistant."}]},
@@ -83,9 +84,17 @@ class DocumentExtractor:
             return result
         return {}
 
-    def get_general_information(self):
-        field_to_extract = self.extraction_fields['general']
-        text_with_field_descriptions = self.get_field_descriptions(fields_of_interest=field_to_extract)
+    def get_general_information(self, pil_img: PIL.Image.Image):
+        fields_to_extract = self.extraction_fields['general']
+        text_with_field_descriptions = self.get_field_descriptions(fields_of_interest=fields_to_extract)
+        json_format = self.get_json_format(field_names=fields_to_extract)
+        prompt_text = "You are provided pages of a bank document PDF file. Identify the following fields:\n"
+        prompt_text += text_with_field_descriptions
+        prompt_text += json_format
+        prompt = self.get_image_prompt(text=prompt_text)
+        information = \
+            self.vision_llm(messages=prompt, images=[pil_img], max_new_tokens=2000)[0]
+        return information
 
     def get_document_spefic_information(self):
         pass
@@ -97,13 +106,14 @@ class DocumentExtractor:
             match = df[df['Field Name'] == field]
             if not match.empty:
                 description = match.iloc[0]['Description']
-                result_string += f"{field}: {description}\n"
+                result_string += f"{field}: {description}.\n"
             else:
                 raise ValueError("Field Description does not exist!")
         return result_string
 
     def get_json_format(self, field_names: list[str]) -> str:
-        result = '```json\n'
+        result = "\nProvide answer in the following JSON format:\n"
+        result += '```json\n'
         for field in field_names:
             json_string = json.dumps({field: f"ESTIMATED_{field.upper()}"})
             result += f'{json_string}\n'
@@ -111,8 +121,19 @@ class DocumentExtractor:
         result += '```'
         return result
 
+    def get_image_prompt(self, text: str):
+        prompt = [
+            {"role": "system", "content": [{"type": "text", "text": "You are a helpful assistant."}]},
+            {"role": "user", "content": [
+                {"type": "image"},
+                {"type": "text",
+                 "text": text}
+            ]}
+        ]
+        return prompt
+
 
 if __name__ == '__main__':
     c = DocumentExtractor()
-    # c.get_general_information()
-    print(c.get_json_format(['document_id', 'document_type']))
+    c.extract(document_path='documents/doc-01.pdf')
+    # print(c.get_json_format(['document_id', 'document_type']))
